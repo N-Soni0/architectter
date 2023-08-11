29cd6d6db24b3bc0e84681ab0de9c4be60e5c0f1
interpure <anton.stecyura.pr1@gmail.com>
2023-08-11 15:26:13 +0300

    Create and load user from user doc from DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useUser } from "./hooks/useUser";
 import AppBar from "./modules/AppBar/AppBar";
 import Router from "./routes/Router";
+import { useUserStore } from "./store/userStore";
 
 
 function App() {
+  const userDoc = useUser();  
+  const signIn = useUserStore(state => state.signIn)
+
+  useEffect(() => {
+    if (!userDoc) return;
+
+    signIn(userDoc);
+  }, [userDoc?._id, signIn])
 
   return (
     <div className="flex min-h-screen flex-col">
---
+++ src/store/userStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+
+interface State {
+    user: Maybe<UserDoc>;
+    isLoading: boolean;
+}
+
+
+interface Actions {
+    signIn: (user: UserDoc) => void;
+    signOut: () => void;
+    setLoading: (isLoading: boolean) => void;
+}
+
+export const useUserStore = create(
+    immer<State & Actions>((set) => ({
+        isLoading: true,
+        user: null,
+
+        setLoading: (isLoading) => {
+            set(store => { store.isLoading = isLoading })
+        },
+
+        signIn: (user) => {
+            set(store => {
+                store.isLoading = false;
+                store.user = user
+            })
+        },
+
+        signOut: () => {
+            set(store => {
+                store.isLoading = false;
+                store.user = null
+            })
+        }
+    }))
+)

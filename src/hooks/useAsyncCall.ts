3952bfbdba129343eac4624320d2a8b32ac868a1
interpure <anton.stecyura.pr1@gmail.com>
2023-08-15 01:05:53 +0300

    Create new custom hook for handling loading and erorrs

---
+++ src/hooks/useAsyncCall.ts
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react"
+
+export const useAsyncCall = <TArgs, R>(asyncFunction: (...args: TArgs[]) => Promise<R>) => {
+    const [result, setResult] = useState<Maybe<R>>(null);
+    const [error, setError] = useState<unknown>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const run = useCallback(async (...args: TArgs[]) => {
+        try {
+            setIsLoading(true);
+            const fetchedResult = await asyncFunction(...args)
+
+            setResult(fetchedResult);
+            return fetchedResult;
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+        
+    }, [asyncFunction])
+
+    return {
+        result,
+        error,
+        isLoading,
+        run
+    }
+}

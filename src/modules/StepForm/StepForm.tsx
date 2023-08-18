a750a9359cc30969010315b492aa10b6d2890ce4
interpure <anton.stecyura.pr1@gmail.com>
2023-08-18 13:43:53 +0300

    Centered StepBar in StepForm module

--- src/modules/StepForm/StepForm.tsx
+++ src/modules/StepForm/StepForm.tsx
@@ -20,7 +20,7 @@ const StepForm: React.FC<StepFormProps> = ({ steps }) => {
 
 	return (
 		<StepControllerProvider stepController={stepController}>
-			<div>
+			<div className='flex flex-col items-center'>
 				<StepBar
 					currentStep={stepController.currentStep}
 					steps={steps.map((step) => ({

47a3db1a34083cb7d50ce23e9a38a7a5cb043b4a
interpure <anton.stecyura.pr1@gmail.com>
2023-08-21 13:40:44 +0300

    Add navigation on edit button click

--- src/modules/ModelsList/components/ModelListItem.tsx
+++ src/modules/ModelsList/components/ModelListItem.tsx
@@ -13,57 +13,57 @@ interface ModelsListItemProps {
 }
 
 const ModelsListItem: React.FC<ModelsListItemProps> = ({ model, onClick }) => {
-    const deleteModalController = usePopupController();
+	const deleteModalController = usePopupController();
 	const navigate = useNavigate();
 
 	const popUpItems: ButtonPopUpItem[] = [
-        {
-            text: 'Edit',
-            icon: <FiEdit className='' />,
-            onClick: () => {
-				navigate(`/model/${model._id}/edit`)
-            },
-        },
-        {
-            text: 'Delete',
-            icon: <AiOutlineDelete className='' />,
-            className: 'text-red-600 font-medium',
-            onClick: () => {
-                deleteModalController.open();
-            },
-        },
-    ];
+		{
+			text: 'Edit',
+			icon: <FiEdit className='' />,
+			onClick: () => {
+				navigate(`/model/${model._id}/edit`);
+			},
+		},
+		{
+			text: 'Delete',
+			icon: <AiOutlineDelete className='' />,
+			className: 'text-red-600 font-medium',
+			onClick: () => {
+				deleteModalController.open();
+			},
+		},
+	];
 
 	return (
-		<>
-			<ListItem className='card bg-base-200 shadow-md flex-shrink-0 w-52 hover:bg-base-300'>
-				<div
-					className='card-body !justify-end static z-0 p-2 cursor-pointer'
-					onClick={() => {
-						onClick && onClick(model)
-					}}
-				>
-					<div className='card-actions w-fit ml-auto justify-end cursor-auto z-40'>
-						<ButtonPopUp
-							className='btn btn-square btn-sm btn-ghost'
-							items={popUpItems}
-						>
-							<FiMoreVertical className='' />
-						</ButtonPopUp>
-					</div>
-
-					<ModelListItemTitle isPrivate={model.private}>{model.name}</ModelListItemTitle>
-
-					<p className='text-xs opacity-30'>{model.address}</p>
+		<ListItem className='card bg-base-200 shadow-md flex-shrink-0 w-52 hover:bg-base-300'>
+			<div
+				className='card-body p-2 cursor-pointer'
+				onClick={() => {
+					onClick && onClick(model);
+				}}
+			>
+				<div className='card-actions w-fit ml-auto justify-end cursor-auto'>
+					<ButtonPopUp
+						className='btn btn-square btn-sm btn-ghost'
+						items={popUpItems}
+					>
+						<FiMoreVertical className='' />
+					</ButtonPopUp>
 				</div>
 
-				<DeleteConfirmModal
-					model={model}
-					close={deleteModalController.close}
-					isOpened={deleteModalController.isOpened}
-				/>
-			</ListItem>
-		</>
+				<ModelListItemTitle isPrivate={model.private}>
+					{model.name}
+				</ModelListItemTitle>
+
+				<p className='text-xs opacity-30'>{model.address}</p>
+			</div>
+
+			<DeleteConfirmModal
+				model={model}
+				close={deleteModalController.close}
+				isOpened={deleteModalController.isOpened}
+			/>
+		</ListItem>
 	);
 };
 

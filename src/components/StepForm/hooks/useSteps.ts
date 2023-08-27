ddf75040870a095d3b309c75781f8f884c26e87d
interpure <anton.stecyura.pr1@gmail.com>
2023-08-27 20:40:43 +0300

    Direction state in useSteps

--- src/components/StepForm/hooks/useSteps.ts
+++ src/components/StepForm/hooks/useSteps.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export const useSteps = (steps: number) => {
     const [currentStep, setCurrentStep] = useState<Maybe<number>>(null);
+    const [direction, setDirection] = useState<'left' | 'right'>('left');
 
     useEffect(() => {
         if (steps < 0) return;
@@ -11,15 +12,18 @@ export const useSteps = (steps: number) => {
 
     const next = () => {
         if (!currentStep || currentStep + 1 > steps) return;
+        setDirection('left')
         setCurrentStep(currentStep + 1);
     }
 
     const prev = () => {
         if (!currentStep || currentStep - 1 <= 0) return;
+        setDirection('right')
         setCurrentStep(currentStep - 1);
     }
 
     return {
+        direction,
         currentStep,
         next, 
         prev,

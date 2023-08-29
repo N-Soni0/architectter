d94922e052553d70fd99bcdbd1135d5b285a30e3
interpure <anton.stecyura.pr1@gmail.com>
2023-08-30 01:02:57 +0300

    Remove initial value in CreateFloorPage

--- src/pages/CreateFloorPage/CreateFloorPage.tsx
+++ src/pages/CreateFloorPage/CreateFloorPage.tsx
@@ -1,31 +1,33 @@
-import { useNavigate, useParams } from "react-router-dom"
-import { createFloor } from "@/api/floors/mutations";
-import { Id } from "@convex/_generated/dataModel";
-import { useMutation, useQueryClient } from "react-query";
-import { QUERY_KEYWORDS } from "@/constants/queryKeywords";
-import { CreateFloorForm } from "@/modules/FloorForm";
+import { useNavigate, useParams } from 'react-router-dom';
+import { createFloor } from '@/api/floors/mutations';
+import { Id } from '@convex/_generated/dataModel';
+import { useMutation, useQueryClient } from 'react-query';
+import { QUERY_KEYWORDS } from '@/constants/queryKeywords';
+import { CreateFloorForm } from '@/modules/FloorForm';
 
 const CreateFloorPage = () => {
-  const navigate = useNavigate();
-  const { modelId } = useParams();
+	const navigate = useNavigate();
+	const { modelId } = useParams();
 	const queryClient = useQueryClient();
 
-  const { mutateAsync } = useMutation(createFloor, {
-    onSuccess: () => {
-      queryClient.invalidateQueries([QUERY_KEYWORDS.FLOORS, modelId]);
-    }
-  })
+	const { mutateAsync } = useMutation(createFloor, {
+		onSuccess: () => {
+			queryClient.invalidateQueries([QUERY_KEYWORDS.FLOORS, modelId]);
+		},
+	});
 
-  return (
-    <div className="container w-full h-full flex items-start justify-center">
-      <CreateFloorForm initial={{ height: 100 }} onSubmit ={async (floor) => {
-        if (!modelId) return;
+	return (
+		<div className='container w-full h-full flex items-start justify-center'>
+			<CreateFloorForm
+				onSubmit={async (floor) => {
+					if (!modelId) return;
 
-        await mutateAsync({ ...floor, model: modelId as Id<'models'> })
-        navigate(`/model/${modelId}`);
-      }} />
-    </div>
-  )
-}
+					await mutateAsync({ ...floor, model: modelId as Id<'models'> });
+					navigate(`/model/${modelId}`);
+				}}
+			/>
+		</div>
+	);
+};
 
-export default CreateFloorPage
+export default CreateFloorPage;

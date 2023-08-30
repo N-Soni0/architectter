183a5a54a50afe9d4c70147c9ce7fb19f13c7d3d
interpure <anton.stecyura.pr1@gmail.com>
2023-08-31 01:23:58 +0300

    Use Component Loader in Button

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from 'tailwind-merge';
-import Loader from './Loader';
+import ComponentLoader from './ComponentLoader';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	isLoading?: boolean;
@@ -20,17 +20,8 @@ const Button: React.FC<ButtonProps> = ({
 			className={twMerge('btn relative', className)}
 			{...props}
 		>
-			{isLoading && (
-				<>
-					<div className='absolute w-full h-full z-20 bg-black bg-opacity-70' />
-
-					{customLoader ? (
-						customLoader
-					) : (
-						<Loader className='loading-md absolute top-1/2 left-1/2 z-50 text-white opacity-50 -translate-x-1/2 -translate-y-1/2' />
-					)}
-				</>
-			)}
+			{isLoading &&
+				(customLoader || <ComponentLoader loaderClassName='loading-md' />)}
 			{children}
 		</button>
 	);

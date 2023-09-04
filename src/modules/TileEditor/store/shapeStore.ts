aee80169311586a59f0d8d025449714772fc090e
interpure <anton.stecyura.pr1@gmail.com>
2023-09-04 22:47:03 +0300

    Remove persisit middleware.

--- src/modules/TileEditor/store/shapeStore.ts
+++ src/modules/TileEditor/store/shapeStore.ts
@@ -1,73 +1,70 @@
-import { create } from "zustand";
-import { persist, createJSONStorage } from 'zustand/middleware'
-import { immer } from "zustand/middleware/immer";
-import { IEditShape } from "../types/editShape";
-import { getTileId } from "../utils/getTileId";
-import { ITile } from "@/types/tile";
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import { IEditShape } from '../types/editShape';
+import { getTileId } from '../utils/getTileId';
+import { ITile } from '@/types/tile';
 
- 
 interface State {
-    shape: IEditShape;
-    helperShapes: IEditShape[];
+	shape: IEditShape;
+	helperShapes: IEditShape[];
 }
 
 interface Actions {
-    init: (shape: IEditShape, helperShapes?: IEditShape[]) => void;
-   
-    addTile: (newTile: ITile) => void;
-    removeTile: (removeTile: ITile) => void;
-    clearTiles: () => void;
-    moveTile: (tile: ITile, newCoordinates: Coordinates<2>) => void;
+	init: (shape: IEditShape, helperShapes?: IEditShape[]) => void;
+
+	addTile: (newTile: ITile) => void;
+	removeTile: (removeTile: ITile) => void;
+	clearTiles: () => void;
+	moveTile: (tile: ITile, newCoordinates: Coordinates<2>) => void;
 }
 
 export const useShapeStore = create(
-    persist(
-        immer<State & Actions>((set) => ({
-            shape: {
-                tiles: [],
-                color: '#661ae6',
-                connect: true,
-                name: 'Editing shape'
-            },
-            helperShapes: [],
+	immer<State & Actions>((set) => ({
+		shape: {
+			tiles: [],
+			color: '#661ae6',
+			connect: true,
+			name: 'Editing shape',
+		},
+		helperShapes: [],
 
-            init: (shape, helperShapes) => {
-                set(store => { 
-                    store.shape = shape 
-                    store.helperShapes = helperShapes ?? []
-                })
-            },
+		init: (shape, helperShapes) => {
+			set((store) => {
+				store.shape = shape;
+				store.helperShapes = helperShapes ?? [];
+			});
+		},
 
-            addTile: (newTile) => {
-                set(store => { store.shape.tiles.push(newTile) })
-            },
+		addTile: (newTile) => {
+			set((store) => {
+				store.shape.tiles.push(newTile);
+			});
+		},
 
-            removeTile: (removeTileID) => {
-                set(store => { 
-                    store.shape.tiles = store.shape.tiles.filter(tile => getTileId(tile) !== getTileId(removeTileID)) 
-                })
-            },
+		removeTile: (removeTileID) => {
+			set((store) => {
+				store.shape.tiles = store.shape.tiles.filter(
+					(tile) => getTileId(tile) !== getTileId(removeTileID)
+				);
+			});
+		},
 
-            clearTiles: () => {
-                set(store => { 
-                    store.shape.tiles = []
-                })
-            },
+		clearTiles: () => {
+			set((store) => {
+				store.shape.tiles = [];
+			});
+		},
 
-            moveTile: (movedTile, newCoordinates) => {
-                set(store => {
-                    const tile = store.shape.tiles.find(tile => getTileId(tile) === getTileId(movedTile));
+		moveTile: (movedTile, newCoordinates) => {
+			set((store) => {
+				const tile = store.shape.tiles.find(
+					(tile) => getTileId(tile) === getTileId(movedTile)
+				);
 
-                    if (tile) {
-                        tile.coordinates = newCoordinates
-                    }
-                })
-            },
-
-        })),
-        {
-            name: 'edited-tiles',
-            storage: createJSONStorage(() => localStorage)
-        }
-    )
-)
+				if (tile) {
+					tile.coordinates = newCoordinates;
+				}
+			});
+		},
+	}))
+);
